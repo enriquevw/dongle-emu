e3f94283e76006b60f0c33bcf4ede42179b8f41b
enriquevw <enrique@latipografica.mx>
2026-06-08 13:58:10 -0600

    Update dongle_emu.c

--- dongle_emu.c
+++ dongle_emu.c
@@ -175,6 +175,17 @@ uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf) {
     return desc_hid_report;
 }
 
+uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id,
+                                hid_report_type_t report_type,
+                                uint8_t* buffer, uint16_t reqlen) {
+    (void)itf;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)reqlen;
+    return 0;
+}
+
 void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id,
                             hid_report_type_t report_type,
                             uint8_t const* buffer, uint16_t bufsize) {
